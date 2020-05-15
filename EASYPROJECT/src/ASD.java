fd7d1757398b8d4f7ef50b9e1de366368eee99e9
Pechenkina652 <nasta-kuprienko@mail.ru>
2020-05-15 23:47:54 +0500

    Изменение кнопки

--- EASYPROJECT/src/ASD.java
+++ EASYPROJECT/src/ASD.java
@@ -1,13 +1,6 @@
 import java.awt.Color;
-import java.awt.Container;
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
